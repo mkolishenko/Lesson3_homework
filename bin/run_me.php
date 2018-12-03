1d3ad98a0de4068025d556771b3dade34d6d0561
Maksym <mkolishenko@gmail.com>
2018-12-03 13:44:00 +0200

    Beta v1.0

--- bin/run_me.php
+++ bin/run_me.php
@@ -7,29 +7,41 @@
 
   $key = 4;
   $value = 'New_Old';
-
+// ------------ Yml section --------------------------------------------------
   $yaml = new KeyValueStore_Yaml();
   $yaml->set($key,$value);
-  //if ($yaml->has($key)){
-  //    printf ("YML Value %s from file \n",$yaml->get($key));
-  //} else{
-   //   printf ("YML Value %s does not exists \n",$key);
-  //}
+  if ($yaml->has($key)){
+      printf ("YML Value %s from file \n",$yaml->get($key));
+  } else{
+      printf ("YML Value %s does not exists \n",$key);
+  }
 //  $yaml->remove($key);
 //  $yaml->clear();
 
+
+
+// ------------ Json section --------------------------------------------------
   $json = new  KeyValueStore_Json();
   $json -> set($key, $value);
-  //if ($json->has($key)){
-  //    printf ("JSON Value %s from file \n",$json->get($key));
-  //} else{
-   //   printf ("JSON Value %s does not exists \n",$key);
-  //}
+  if ($json->has($key)){
+      printf ("JSON Value %s from file \n",$json->get($key));
+  } else{
+      printf ("JSON Value %s does not exists \n",$key);
+  }
 //  $json->remove($key);
 //  $json->clear();
 
+
+// ------------ Array section --------------------------------------------------
   $varray = new  KeyValueStore_Array();
   $varray -> set($key, $value);
+//  $varray ->remove($key);
+  if ($varray->has($key)){
+      printf ("Array Value %s \n",$varray->get($key));
+  } else{
+     printf ("Array Value %s does not exists \n",$key);
+  }
+  //$varray ->clear();
 
 
 
